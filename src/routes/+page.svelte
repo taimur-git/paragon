132a7b252f6f21e878f3d5831bed0aa57d089ba4
Taimur Rahman <taimurrahman98@gmail.com>
2023-06-01 09:21:53 +0600

    start svelte+prisma project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	import Counter from './Counter.svelte';
+	import welcome from '$lib/images/svelte-welcome.webp';
+	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import { PrismaClient } from '@prisma/client';
+	import Header from './Header.svelte';
+	const prisma = new PrismaClient();
+
+	async function main() {
+/*
+	// ... you will write your Prisma Client queries here
+		const user = await prisma.user.create({
+			data: {
+				name: 'Alice',
+				email: 'alice@prisma.io',
+			},
+		})
+			console.log(user)
+			*/
+		
+	}
+	main()
+		.then(async () => {
+			await prisma.$disconnect()
+		})
+		.catch(async (e) => {
+			console.error(e)
+			await prisma.$disconnect()
+			process.exit(1)
+		})
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+
+<!--<h1>{user}</h1>
+	<h1>
+		<span class="welcome">
+			<picture>
+				<source srcset={welcome} type="image/webp" />
+				<img src={welcome_fallback} alt="Welcome" />
+			</picture>
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+
+	<Counter />
+-->
+</section>
+
+<style>
+
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+/*
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+	*/
+</style>
