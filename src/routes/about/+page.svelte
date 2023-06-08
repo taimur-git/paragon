aaa39ad89bf1d09ef6c8d2c6a342416b9b730631
Taimur Rahman <taimurrahman98@gmail.com>
2023-06-08 13:52:52 +0600

    cleaned up the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,25 +1,12 @@
-<script lang>
-	/*
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-	
-	import Header from './Header.svelte';
-	*/
+<script lang='ts'>
 	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
-//import type { TableSource } from '@skeletonlabs/skeleton';
+	import type { PageData } from './$types';
 /** @type {import('./$types').PageData} */
-export let data;
-
+export let data: PageData;
 	$: ({ users } = data)
 	
-
 	let valueSingle = 'users';
-	
-
 </script>
-
-
 
 <svelte:head>
 	<title>Home</title>
@@ -31,15 +18,6 @@ export let data;
 	<input type='text' name='email'/>
 	<button type='submit'>Create User</button>
 </form>
-
-
-<!--p>
-	{user.name} | {user.email}
-</p>
-<form action="?/deleteUser&id={user.id}" method="POST">
-	<button type="submit">Delete user</button>
-</form-->
-
 
 <ListBox>
 	{#each users as user}
@@ -55,32 +33,4 @@ export let data;
 
 <style>
 
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-/*
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-	*/
 </style>
