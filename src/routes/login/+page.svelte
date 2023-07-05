d0f82e069abb58552f37c8322a1f10ba09ea6415
AsifUchchas <90467725+Asif-Uddin-Ahmed@users.noreply.github.com>
2023-07-06 02:42:15 +0600

    added login and changes in the header, edited the style s
    Added login functionalities and changes in the header file. Also added style to the login and register options

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,25 +1,15 @@
-<form>
+<form method="POST">
     <hgroup>
-        <h1>Register</h1>
-        <h3>To post ads, you need an account.</h3>
-        
+        <h1>Login</h1>
+        <h3>Enter your credentials to login.</h3>
     </hgroup>
     <label for="username">Username</label>
     <input type="text" id="username" name="username" placeholder="Username" required>
-
-    <label for="email">Email</label>
-    <input type="email" id="email" name="email" placeholder="Email" required>
-
     <label for="password">Password</label>
     <input type="password" id="password" name="password" placeholder="Password" required>
-
-    <label for="password">Confirm Password</label>
-    <input type="password" id="password" name="password" placeholder="Password" required>
-
-    <button type="submit">Register</button>
+    <button type="submit">Login</button>
 </form>
-<p>Already have an account? <a href="/login">Login</a></p>
-
+<p>Don't have an account? <a href="/register">Register</a></p>
 <style>
     form {
         display: flex;
@@ -70,17 +60,24 @@
     button {
         width: 100%;
         height: 3rem;
-        border: 1px solid #ccc;
+        border: none;
         border-radius: 0.5rem;
-        padding: 0.5rem;
+        background-color: #000;
+        color: #fff;
+        font-size: 1.5rem;
+        font-weight: 400;
+        margin-bottom: 1rem;
+    }
+
+    p {
         font-size: 1.5rem;
         font-weight: 400;
         margin-bottom: 1rem;
     }
 
     a {
+        color: #000;
         font-size: 1.5rem;
         font-weight: 400;
-        color: #000;
     }
 </style>
